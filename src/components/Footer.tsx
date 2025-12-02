398a6ef201a79d3602e632042c09414d2532c247
david ojo <ojodavid115@gmail.com>
2025-12-02 13:50:40 +0100

    Remove commented-out code from Footer component for cleaner implementation.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -294,7 +294,6 @@ const Footer = () => {
         </div>
       </div>
 
-      {/* Bottom Footer */}
       <div className="border-t border-gray-700">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
           <div className="flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
